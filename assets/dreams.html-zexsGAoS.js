bd90b37a800e47e8060e572d764ecce1018b2187
liz-starfield <42017051+liz-starfield@users.noreply.github.com>
2024-07-31 10:57:37 +0000

    Deploying to gh-pages from @ liz-starfield/blog@0dd2ef78bb5bd1f066d690b6a8bd7bac55e0dd35 🚀

---
+++ assets/dreams.html-zexsGAoS.js
@@ -0,0 +1,15 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as i,f as d,a as e,b as s,e as r}from"./app-1nBkCHNn.js";const t={},c=e("h1",{id:"dreams-and-wishes",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#dreams-and-wishes","aria-hidden":"true"},"#"),s(" Dreams and Wishes")],-1),l=r(`<h2 id="_1-english" tabindex="-1"><a class="header-anchor" href="#_1-english" aria-hidden="true">#</a> 1. English</h2><p>What do you dream of? What do you wish for?</p><h2 id="_2-german" tabindex="-1"><a class="header-anchor" href="#_2-german" aria-hidden="true">#</a> 2. German</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>Was wünschst du dir? – What do you wish for? (informal)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>Ich wünsche mir Gesundheit. – I want good health.
+Ich wünsche mir Geld. – I want money.
+Ich wünsche mir ein neues Auto. – I want a new car.
+Ich wünsche mir Frieden auf der Welt. – I want world peace.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>Ich möchte eine Weltreise machen. – I&#39;d like to take a trip around the world.
+Ich möchte ein Café eröffnen. – I&#39;d like to open a cafe.
+Ich möchte studieren. – I&#39;d like to go to university.
+Ich würde gerne eine Firma gründen. – I&#39;d like to start a company.
+Ich würde gerne ein Buch schreiben. – I&#39;d like to write a book.
+
+Ich möchte einen guten Job. – I&#39;d like a good job.
+Ich träume von einem besseren Leben. – I dream of a better life.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code> Dreams and wishes - Träume und Wünsche 
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,7);function o(h,m){return a(),i("div",null,[c,d(" more "),l])}const b=n(t,[["render",o],["__file","dreams.html.vue"]]);export{b as default};
