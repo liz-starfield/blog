6e5a505a0b3677349b9fc06be72e1924272e1d6e
liz-starfield <42017051+liz-starfield@users.noreply.github.com>
2024-08-08 14:55:33 +0000

    Deploying to gh-pages from @ liz-starfield/blog@b7c76328b58f7c79496cc0b7bb66f0e2a5445537 🚀

---
+++ assets/feelings.html-rLZdE5Sp.js
@@ -0,0 +1,90 @@
+import{_ as i}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as l,c as n,f as a,a as e,b as s,e as d}from"./app-7SIUZpS0.js";const r={},t=e("h1",{id:"feelings",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#feelings","aria-hidden":"true"},"#"),s(" Feelings")],-1),o=d(`<h2 id="_1-english" tabindex="-1"><a class="header-anchor" href="#_1-english" aria-hidden="true">#</a> 1. English</h2><p>feel energetic 感到很有活力</p><p>feel refreshed 感到头脑更清醒</p><p>feel motivated 感觉很有动力</p><p>feel calm and peaceful 感觉心情很平静</p><p>Note：feel energetic和feel calm and peaceful常用来对比</p><p>Outdoor activities make people feel energetic.户外活动让人们感觉很有活力。</p><p>A good night&#39;s sleep makes me feel refreshed.好好睡一觉让我感觉头脑更清醒。</p><p>Employees feel motivated when they have a sense of belonging.当员工们有归属感时，他们感觉很有动力。</p><p>Colours can change people&#39;s moods. For example, red and orange make people feel energetic, while green and light blue make people feel calm and peaceful.</p><h3 id="_1-1-happy" tabindex="-1"><a class="header-anchor" href="#_1-1-happy" aria-hidden="true">#</a> 1.1. Happy</h3><ul><li>“Happy” Synonyms <ul><li>happy</li><li>pleased</li><li>excited</li><li>beyond excited</li><li>thrilled</li><li>overjoyed</li><li>jubilant</li><li>elated</li><li>ecstatic</li></ul></li><li>Slang phrases for “Happy” <ul><li>over the moon</li><li>on cloud nine</li><li>in seventh heaven</li><li>happy as a clam</li><li>tickled pink</li><li>levitating</li><li>chuffed</li></ul></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>I&#39;m pleased with my life.
+This news thrills me to bits.
+I&#39;m feeling overjoyed.
+
+I&#39;m on cloud nine!
+They&#39;re over the moon with you.
+This makes gim happy as a clam.
+I&#39;m chuffed to bits!
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="_1-2-sad" tabindex="-1"><a class="header-anchor" href="#_1-2-sad" aria-hidden="true">#</a> 1.2. Sad</h3><ul><li>basic <ul><li>sad /sadness</li><li>upset</li><li>frustrated</li><li>sorrowful / sorrow</li><li>disappointed / disappointment</li><li>hurt</li><li>heartbreak / heartbroken / broken-hearted</li><li>lonely / loneliness</li><li>neglected</li><li>uncomfortable</li><li>melancholy</li></ul></li><li>advanced <ul><li>depressed / depressed</li><li>clinical depression</li><li>ennui</li><li>grief / loss / mourning</li></ul></li><li>slang <ul><li>blue</li><li>down</li><li>in the dumps</li></ul></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>I&#39;m really sad.
+He&#39;s full of sadness.
+I met a lot of disappointment this year.
+He&#39;s trying to deal with heartbreak.
+She&#39;s a melancholy person.
+I feel hurt when you say my cooking tastes bad.
+I feel neglected when you don&#39;t call all week.
+
+She is grieving her friend.
+I&#39;m  feeling pretty depressed today.
+I suffer from clinical depression.
+
+I&#39;m feeling pretty blue lately.
+He&#39;s pretty down about his test.
+She&#39;s been in the dumps lately.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Conversations:</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>A: What’s up? You look down. 
+B: Nothing. I’m just disappointed. 
+A: Disappointed? About what?
+B: College just isn’t what I thought it would be.
+
+A: I suffer from depression. 
+B: Don’t be sad! Look on the bright side!
+A: No! I mean, I have clinical depression. B: Oh! Well, that’s a different story.
+
+A: How’ve you been?
+B: Honestly, I’m pretty blue these days. 
+A: Is there any reason why?
+B: I still haven’t gotten over the loss of my cat.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="_1-3-like" tabindex="-1"><a class="header-anchor" href="#_1-3-like" aria-hidden="true">#</a> 1.3. Like</h3><ul><li>verbs <ul><li>like</li><li>fancy</li><li>admire</li><li>appreciate</li><li>cherish</li><li>treasure</li><li>prize</li><li>value</li></ul></li><li>phrases <ul><li>be fond of</li><li>find joy in</li><li>have feeling for</li><li>have a soft spot for</li><li>can&#39;t do without</li></ul></li><li>slang <ul><li>my cup of tea</li><li>my jam</li><li>my vibe</li><li>I&#39;m digging xxx.</li><li>It&#39;s a vibe.</li><li>I&#39;m vibing with xxx.</li><li>I had a great/wonderful time.</li><li>I had a ball.</li><li>I had a blast.</li></ul></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>I like it.
+I cherish out time together.
+He admires her.
+She fancies him.
+I treasure my pets.
+
+I&#39;m fond of fast cars.
+She finds a lot of joy in hiking.
+I can&#39;t do without my morning coffee.
+I think I have feelings for Steven.
+He has a soft spot for dogs.
+
+He&#39;s my cup of tea.
+English is my jam!
+I&#39;m vibing with this song.
+She&#39;s digging the art.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>Conversations:</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>A: What kind of food do you like?
+B: I’m pretty fond of Asian food. 
+A: Are you into Thai food?
+B: Thai food is a WHOLE vibe!
+
+A: What do you think about pop music?
+B: It’s not really my jam. 
+A: Then what are you into?
+B: I’m really vibing with rap these days.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="_1-4-dislike" tabindex="-1"><a class="header-anchor" href="#_1-4-dislike" aria-hidden="true">#</a> 1.4. Dislike</h3><ul><li>it&#39;s not my thing.</li><li>It&#39;s not my cup of tea</li><li>I&#39;m not a fan of it.</li><li>I don&#39;t have any talent for it.</li></ul><h3 id="_1-5-relax" tabindex="-1"><a class="header-anchor" href="#_1-5-relax" aria-hidden="true">#</a> 1.5. Relax</h3><ul><li>relax</li><li>unwind</li><li>It&#39;s such a great way to relax.</li><li>It&#39;s such a great way to unwind.</li><li>It&#39;s such a great way to reduce stress.</li><li>It&#39;s a really good mood-lifter.</li><li>It&#39;s very relaxing and enjoyable.</li><li>It helps me let go of my stress.</li><li>It really helps me loosen up.</li><li>It helps me clear my head.</li><li>It helps me take my mind off things.</li></ul><h3 id="_1-6-boring" tabindex="-1"><a class="header-anchor" href="#_1-6-boring" aria-hidden="true">#</a> 1.6. Boring</h3><ul><li>boring</li><li>quite boring</li><li>dull /dʌl/</li><li>as dull as dishwasher</li><li>It takes up a lot of my time.</li><li>It is so time-consuming.</li></ul><h3 id="_1-7-anger" tabindex="-1"><a class="header-anchor" href="#_1-7-anger" aria-hidden="true">#</a> 1.7. Anger</h3><ul><li>adj. <ul><li>mad (at)</li><li>angry (at/with)</li><li>annoyed (at/with/by)</li><li>upset (with/by)</li><li>enraged (at/by)</li></ul></li><li>phrase <ul><li>get to sbd</li><li>tick sbd off</li><li>pop off</li><li>get on sbd&#39;s nerves</li><li>lose sbd&#39;s temper</li><li>blow sbd&#39;s top</li></ul></li></ul><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>I&#39;m mad.
+He makes me mad.
+She upsets me.
+Work make him upset.
+I&#39;m amgry at you.
+I&#39;m annoyed by that sound.
+
+My boss gets on my nerves.
+He ticks her off.
+She&#39;s pretty ticked off.
+His voice just gets to me.
+She popped off on him.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="_2-german" tabindex="-1"><a class="header-anchor" href="#_2-german" aria-hidden="true">#</a> 2. German</h2><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>das Gefühl, die Gefühle – feeling
+das Glück – happiness(nur Singular)
+
+Ich freue mich! – I&#39;m happy!
+Ich muss lachen. – I have to laugh.
+die Trauer – mourning(nur Singular)
+
+Ich bin traurig. – I&#39;m sad.
+Ich muss weinen. – I have to cry.
+Ich bin wütend. – I&#39;m angry.
+Ich habe Angst. – I&#39;m scared.
+Ich bin geschockt. – I&#39;m shocked.
+Ich bin überrascht. – I&#39;m surprised.
+Ich schäme mich. – I&#39;m ashamed.
+Ich bin verwirrt. – I&#39;m confused.
+Mir ist langweilig. – I&#39;m bored.
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,34);function u(c,v){return l(),n("div",null,[t,a(" more "),o])}const b=i(r,[["render",u],["__file","feelings.html.vue"]]);export{b as default};
